4a7abaa7185294a896236d5e0f29f6e66773a040
aidas0Baranauskas <aidas0baranauskas@gmail.com>
2023-03-05 15:41:54 +0200

    chalk test

---
+++ notes-app/app.js
@@ -0,0 +1,16 @@
+// const chalk = require('chalk')
+// const validator = require('validator')
+// const getNotes = require('./notes.js')
+
+// const msg = getNotes()
+// console.log(msg)
+
+// import chalk from 'chalk'
+
+// console.log(chalk.blue('Hello world!'))
+// console.log(validator.isEmail("ajdas112@gmail.com"))
+
+
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hello world!'));
